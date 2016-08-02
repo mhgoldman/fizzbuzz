b621afeb0f24034de5bf8a9de8d27211da7010bb
Martin Goldman <mgoldman@skytap.com>
2016-08-02 08:41:03 -0400

    fizzbuzz refactor	:

--- app.js
+++ app.js
@@ -1,12 +1,45 @@
+'use strict'
+
 $(document).ready(function(){
-  for (i=1; i<=100; i++) {
-    str = ''
-    if (i % 3 == 0) {
+  var num;
+
+  do {
+    var numStr = prompt("Enter a number");
+    num = parseInt(numStr);
+
+  } while (isNaN(num) && numStr !== null);
+
+  if (num !== null) {
+    var output = fizzbuzz(num);
+    $('body').append(output);
+  } 
+});
+
+function fizzbuzz(topNumber) {
+  var str = '';
+
+  for (var i=1; i<=topNumber; i++) {
+    str += fizzbuzzFor(i) + "<br/>";
+  }
+
+  return str;
+
+  function fizzbuzzFor(number) {
+    var str = '';
+
+    if (number % 3 == 0) {
       str += "fizz";
     }
-    if (i % 5 == 0) {
+    if (number % 5 == 0) {
       str += "buzz";
     }
-    $('body').append((str || i) + "<br/>");
+
+    if (str == '')  {
+      str = number;
+    }
+
+    return str;
   }
-});
+  
+}
+
